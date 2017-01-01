c2858bbe437f1ce9c91c1baf2fad4e05675f12a6
Gyuwon Yi <gyuwon@live.com>
2017-01-02 02:59:04 +0900

    IMessageBus 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing/Messaging/IMessageBus.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IMessageBus
+    {
+        Task Send(Envelope message);
+
+        Task SendBatch(IEnumerable<Envelope> messages);
+    }
+}
