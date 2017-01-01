11eefca5142dec9ecb224e0bba1b83b6f6d1fba4
Gyuwon Yi <gyuwon@live.com>
2017-01-01 20:34:41 +0900

    EventStoreDbContext 클래스를 추가합니다.

---
+++ source/RA.EventSourcing/RA.EventSourcing.Sql/EventSourcing/Sql/EventStoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Sql
+{
+    using System.Data.Entity;
+
+    public class EventStoreDbContext : DbContext
+    {
+        public EventStoreDbContext()
+        {
+        }
+
+        public EventStoreDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+
+        public DbSet<Aggregate> Aggregates { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<PendingEvent> PendingEvents { get; set; }
+    }
+}
