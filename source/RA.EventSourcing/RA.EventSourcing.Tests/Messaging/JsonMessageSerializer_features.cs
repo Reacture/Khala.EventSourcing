9918c286dd7373ab5d991f702b183c50d503f4d7
Gyuwon Yi <gyuwon@live.com>
2017-01-01 17:12:06 +0900

    JsonMessageSerializer 클래스를 추가합니다.

---
+++ source/RA.EventSourcing/RA.EventSourcing.Tests/Messaging/JsonMessageSerializer_features.cs
@@ -0,0 +1,52 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Idioms;
+
+    [TestClass]
+    public class JsonMessageSerializer_features
+    {
+        private IFixture fixture = new Fixture();
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Deserialize_has_guard_clause()
+        {
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(JsonMessageSerializer).GetMethod(
+                nameof(JsonMessageSerializer.Deserialize)));
+        }
+
+        public class MutableMessage
+        {
+            public Guid GuidProp { get; set; }
+
+            public int Int32Prop { get; set; }
+
+            public double DoubleProp { get; set; }
+
+            public string StringProp { get; set; }
+        }
+
+        [TestMethod]
+        public void Deserialize_restores_mutable_message_correctly()
+        {
+            // Arrange
+            var sut = new JsonMessageSerializer();
+            var message = fixture.Create<MutableMessage>();
+            string serialized = sut.Serialize(message);
+            TestContext.WriteLine("{0}", serialized);
+
+            // Act
+            object actual = sut.Deserialize(serialized);
+
+            // Assert
+            actual.Should().BeOfType<MutableMessage>();
+            actual.ShouldBeEquivalentTo(message);
+        }
+    }
+}
---
+++ source/RA.EventSourcing/RA.EventSourcing/Messaging/JsonMessageSerializer.cs
@@ -0,0 +1,47 @@
+﻿namespace ReactiveArchitecture.Messaging
+{
+    using System;
+    using System.IO;
+    using Newtonsoft.Json;
+
+    public class JsonMessageSerializer
+    {
+        private readonly JsonSerializer _serializer;
+
+        public JsonMessageSerializer()
+        {
+            _serializer = JsonSerializer.Create(new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Objects,
+#if DEBUG
+                Formatting = Formatting.Indented
+#else
+                Formatting = Formatting.None
+#endif
+            });
+        }
+
+        public string Serialize(object message)
+        {
+            using (var writer = new StringWriter())
+            {
+                _serializer.Serialize(writer, message);
+                return writer.ToString();
+            }
+        }
+
+        public object Deserialize(string json)
+        {
+            if (json == null)
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
+
+            using (var reader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                return _serializer.Deserialize(jsonReader);
+            }
+        }
+    }
+}
