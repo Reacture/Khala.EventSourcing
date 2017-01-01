59f82a59e7dd52e946f143de6415bf5f23a7f890
Gyuwon Yi <gyuwon@live.com>
2017-01-02 03:10:51 +0900

    IEventPublisher 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing/EventSourcing/IEventPublisher.cs
@@ -0,0 +1,13 @@
+﻿namespace ReactiveArchitecture.EventSourcing
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IEventPublisher
+    {
+        Task PublishPendingEvents<T>(Guid sourceId)
+            where T : class, IEventSourced;
+
+        void EnqueueAll();
+    }
+}
