77a1d55ea10fc80e727fa1d5791f94292751abc6
Gyuwon Yi <gyuwon@live.com>
2017-01-03 00:06:41 +0900

    IUniqueIndexedEvent 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing/EventSourcing/Sql/IUniqueIndexedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Sql
+{
+    using System.Collections.Generic;
+
+    public interface IUniqueIndexedEvent
+    {
+        IReadOnlyDictionary<string, string> UniqueIndexedProperties { get; }
+    }
+}
