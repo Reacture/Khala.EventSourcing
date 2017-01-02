75f6acfebca098e7a63ab2636d833268bcffd1b2
Gyuwon Yi <gyuwon@live.com>
2017-01-02 14:39:41 +0900

    IEventSourcedRepository<T> 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing/EventSourcing/IEventSourcedRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ReactiveArchitecture.EventSourcing
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IEventSourcedRepository<T>
+        where T : class, IEventSourced
+    {
+        Task Save(T source);
+
+        Task<T> Find(Guid sourceId);
+    }
+}
