ddad5aa583fd07af372090f638e6e0684a05c99e
Gyuwon Yi <gyuwon@live.com>
2017-01-02 23:45:39 +0900

    ISqlEventSourcedRepository<T> 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing/EventSourcing/Sql/ISqlEventSourcedRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Sql
+{
+    using System.Threading.Tasks;
+
+    public interface ISqlEventSourcedRepository<T>
+        where T : class, IEventSourced
+    {
+        Task<T> FindByUniqueIndexedProperty(string name, string value);
+    }
+}
