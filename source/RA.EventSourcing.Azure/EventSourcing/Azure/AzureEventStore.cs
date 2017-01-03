b837fe9da28dea82c6ddf946e367101264f53771
Gyuwon Yi <gyuwon@live.com>
2017-01-03 21:40:20 +0900

    AzureEventStore 클래스 프레임을 추가합니다.

---
+++ source/RA.EventSourcing.Azure/EventSourcing/Azure/AzureEventStore.cs
@@ -0,0 +1,34 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Azure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using EventSourcing;
+
+    public class AzureEventStore : IEventStore
+    {
+        public Task SaveEvents<T>(IEnumerable<IDomainEvent> events)
+            where T : class, IEventSourced
+        {
+            if (events == null)
+            {
+                throw new ArgumentNullException(nameof(events));
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IDomainEvent>> LoadEvents<T>(
+            Guid sourceId, int afterVersion = default(int))
+            where T : class, IEventSourced
+        {
+            if (sourceId == Guid.Empty)
+            {
+                throw new ArgumentException(
+                    $"{sourceId} cannot be empty", nameof(sourceId));
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ source/RA.EventSourcing.Tests/EventSourcing/Azure/AzureEventStore_features.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Idioms;
+
+namespace ReactiveArchitecture.EventSourcing.Azure
+{
+    [TestClass]
+    public class AzureEventStore_features
+    {
+        private AzureEventStore sut;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sut = new AzureEventStore();
+        }
+
+        [TestMethod]
+        public void sut_implements_IEventStore()
+        {
+            sut.Should().BeAssignableTo<IEventStore>();
+        }
+
+        [TestMethod]
+        public void class_has_guard_clauses()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(AzureEventStore));
+        }
+    }
+}
