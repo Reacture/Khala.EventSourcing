692304148c8b1b87b83d283c4e7ec0736bed2c5e
Gyuwon Yi <gyuwon@live.com>
2017-01-03 20:34:35 +0900

    SqlEventSourcedRepository<T> 클래스를 추가합니다.

---
+++ source/RA.EventSourcing.Sql/EventSourcing/Sql/SqlEventSourcedRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Sql
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public class SqlEventSourcedRepository<T> :
+        EventSourcedRepository<T>,
+        ISqlEventSourcedRepository<T>
+        where T : class, IEventSourced
+    {
+        private readonly ISqlEventStore _eventStore;
+
+        public SqlEventSourcedRepository(
+            ISqlEventStore eventStore,
+            IEventPublisher eventPublisher,
+            Func<Guid, IEnumerable<IDomainEvent>, T> factory)
+            : base(eventStore, eventPublisher, factory)
+        {
+            _eventStore = eventStore;
+        }
+
+        public Task<Guid?> FindIdByUniqueIndexedProperty(
+            string name, string value)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return _eventStore.FindIdByUniqueIndexedProperty<T>(name, value);
+        }
+    }
+}
---
+++ source/RA.EventSourcing.Tests/EventSourcing/Sql/SqlEventSourcedRepository_features.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit2;
+using ReactiveArchitecture.FakeDomain;
+using Xunit;
+
+namespace ReactiveArchitecture.EventSourcing.Sql
+{
+    public class SqlEventSourcedRepository_features
+    {
+        [Fact]
+        public void class_inherits_EventSourcedRepositoryT()
+        {
+            typeof(SqlEventSourcedRepository<>)
+                .BaseType
+                .GetGenericTypeDefinition()
+                .Should().Be(typeof(EventSourcedRepository<>));
+        }
+
+        [Fact]
+        public void sut_implements_ISqlEventSourcedRepository()
+        {
+            var sut = new SqlEventSourcedRepository<FakeUser>(
+                Mock.Of<ISqlEventStore>(),
+                Mock.Of<IEventPublisher>(),
+                Mock.Of<Func<Guid, IEnumerable<IDomainEvent>, FakeUser>>());
+            sut.Should().BeAssignableTo<ISqlEventSourcedRepository<FakeUser>>();
+        }
+
+        [Fact]
+        public void class_has_guard_clauses()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(SqlEventSourcedRepository<>));
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task FindIdByUniqueIndexedProperty_relays_to_event_store(
+            string name,
+            string value,
+            Guid? expected)
+        {
+            var eventStore = Mock.Of<ISqlEventStore>(
+                x =>
+                x.FindIdByUniqueIndexedProperty<FakeUser>(name, value) == Task.FromResult(expected));
+            var sut = new SqlEventSourcedRepository<FakeUser>(
+                eventStore,
+                Mock.Of<IEventPublisher>(),
+                Mock.Of<Func<Guid, IEnumerable<IDomainEvent>, FakeUser>>());
+
+            Guid? actual = await sut.FindIdByUniqueIndexedProperty(name, value);
+
+            actual.Should().Be(expected);
+        }
+    }
+}
