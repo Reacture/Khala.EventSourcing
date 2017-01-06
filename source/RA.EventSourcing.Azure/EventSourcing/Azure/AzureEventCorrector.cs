fb699013aabcf4758533b3fa036feb85374ab4f5
Gyuwon Yi <gyuwon@live.com>
2017-01-07 01:38:53 +0900

    IAzureEventCorrector 구현체를 추가합니다.

---
+++ source/RA.EventSourcing.Azure/EventSourcing/Azure/AzureEventCorrector.cs
@@ -0,0 +1,174 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Azure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Messaging;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using static Microsoft.WindowsAzure.Storage.Table.QueryComparisons;
+    using static Microsoft.WindowsAzure.Storage.Table.TableOperators;
+    using static Microsoft.WindowsAzure.Storage.Table.TableQuery;
+
+    public class AzureEventCorrector : IAzureEventCorrector
+    {
+        private readonly CloudTable _eventTable;
+        private readonly JsonMessageSerializer _serializer;
+        private readonly IMessageBus _messageBus;
+
+        public AzureEventCorrector(
+            CloudTable eventTable,
+            JsonMessageSerializer serializer,
+            IMessageBus messageBus)
+        {
+            if (eventTable == null)
+            {
+                throw new ArgumentNullException(nameof(eventTable));
+            }
+
+            if (serializer == null)
+            {
+                throw new ArgumentNullException(nameof(serializer));
+            }
+
+            if (messageBus == null)
+            {
+                throw new ArgumentNullException(nameof(messageBus));
+            }
+
+            _eventTable = eventTable;
+            _serializer = serializer;
+            _messageBus = messageBus;
+        }
+
+        public Task CorrectEvents<T>(Guid sourceId)
+            where T : class, IEventSourced
+        {
+            if (sourceId == Guid.Empty)
+            {
+                throw new ArgumentException(
+                    $"{sourceId} cannot be empty.", nameof(sourceId));
+            }
+
+            return Correct<T>(sourceId);
+        }
+
+        private async Task Correct<T>(Guid sourceId)
+            where T : class, IEventSourced
+        {
+            List<PendingEventTableEntity> pendingEvents = await GetPendingEvents<T>(sourceId);
+            List<IDomainEvent> domainEvents = RestoreDomainEvents<T>(pendingEvents);
+            await InsertUnpersistedEvents<T>(sourceId, domainEvents);
+            await SendPendingEvents(domainEvents);
+            await DeletePendingEvents(pendingEvents);
+        }
+
+        private async Task<List<PendingEventTableEntity>> GetPendingEvents<T>(
+            Guid sourceId)
+            where T : class, IEventSourced
+        {
+            var query = new TableQuery<PendingEventTableEntity>();
+
+            string filter = GenerateFilterCondition(
+                nameof(ITableEntity.PartitionKey),
+                Equal,
+                PendingEventTableEntity.GetPartitionKey(typeof(T), sourceId));
+
+            return new List<PendingEventTableEntity>(
+                await ExecuteQuery(query.Where(filter)));
+        }
+
+        private List<IDomainEvent> RestoreDomainEvents<T>(
+            List<PendingEventTableEntity> pendingEvents)
+            where T : class, IEventSourced
+        {
+            return pendingEvents
+                .Select(e => e.PayloadJson)
+                .Select(_serializer.Deserialize)
+                .Cast<IDomainEvent>()
+                .ToList();
+        }
+
+        private async Task<List<EventTableEntity>> GetPersistedEvents<T>(
+            Guid sourceId,
+            int version)
+            where T : class, IEventSourced
+        {
+            var query = new TableQuery<EventTableEntity>();
+
+            string filter = CombineFilters(
+                GenerateFilterCondition(
+                    nameof(ITableEntity.PartitionKey),
+                    Equal,
+                    EventTableEntity.GetPartitionKey(typeof(T), sourceId)),
+                And,
+                GenerateFilterCondition(
+                    nameof(ITableEntity.RowKey),
+                    GreaterThanOrEqual,
+                    EventTableEntity.GetRowKey(version)));
+
+            return new List<EventTableEntity>(
+                await ExecuteQuery(query.Where(filter)));
+        }
+
+        private async Task InsertUnpersistedEvents<T>(
+            Guid sourceId,
+            List<IDomainEvent> domainEvents)
+            where T : class, IEventSourced
+        {
+            List<EventTableEntity> persistedEvents = await
+                GetPersistedEvents<T>(sourceId, domainEvents.First().Version);
+
+            IEnumerable<IDomainEvent> unpersistedEvents =
+                domainEvents.Skip(persistedEvents.Count);
+
+            var batch = new TableBatchOperation();
+
+            foreach (IDomainEvent @event in unpersistedEvents)
+            {
+                var entity =
+                    EventTableEntity.FromDomainEvent<T>(@event, _serializer);
+                batch.Insert(entity);
+            }
+
+            await _eventTable.ExecuteBatchAsync(batch);
+        }
+
+        private Task SendPendingEvents(List<IDomainEvent> domainEvents)
+        {
+            return _messageBus.SendBatch(domainEvents);
+        }
+
+        private Task DeletePendingEvents(
+            List<PendingEventTableEntity> pendingEvents)
+        {
+            var batch = new TableBatchOperation();
+
+            foreach (PendingEventTableEntity @event in pendingEvents)
+            {
+                batch.Delete(@event);
+            }
+
+            return _eventTable.ExecuteBatchAsync(batch);
+        }
+
+        private async Task<IEnumerable<TEntity>> ExecuteQuery<TEntity>(
+            TableQuery<TEntity> query)
+            where TEntity : ITableEntity, new()
+        {
+            var entities = new List<TEntity>();
+            TableContinuationToken continuation = null;
+
+            do
+            {
+                TableQuerySegment<TEntity> segment = await
+                    _eventTable.ExecuteQuerySegmentedAsync(query, continuation);
+                entities.AddRange(segment);
+                continuation = segment.ContinuationToken;
+            }
+            while (continuation != null);
+
+            return entities;
+        }
+    }
+}
---
+++ source/RA.EventSourcing.Tests/EventSourcing/Azure/AzureEventCorrector_features.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using Microsoft.WindowsAzure.Storage.Table;
+using Moq;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Idioms;
+using ReactiveArchitecture.FakeDomain;
+using ReactiveArchitecture.FakeDomain.Events;
+using ReactiveArchitecture.Messaging;
+
+namespace ReactiveArchitecture.EventSourcing.Azure
+{
+    [TestClass]
+    public class AzureEventCorrector_features
+    {
+        private static CloudStorageAccount s_storageAccount;
+        private static CloudTable s_eventTable;
+        private static bool s_storageEmulatorConnected;
+        private IFixture fixture;
+        private JsonMessageSerializer serializer;
+        private IMessageBus messageBus;
+        private AzureEventCorrector sut;
+
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            try
+            {
+                s_storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+                CloudTableClient tableClient = s_storageAccount.CreateCloudTableClient();
+                s_eventTable = tableClient.GetTableReference("AzureEventCorrectorTestEventStore");
+                s_eventTable.DeleteIfExists(new TableRequestOptions { RetryPolicy = new NoRetry() });
+                s_eventTable.Create();
+                s_storageEmulatorConnected = true;
+            }
+            catch (StorageException exception)
+            when (exception.InnerException is WebException)
+            {
+                context.WriteLine("{0}", exception);
+            }
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (s_storageEmulatorConnected == false)
+            {
+                Assert.Inconclusive("Could not connect to Azure Storage Emulator. See the output for details. Refer to the following URL for more information: http://go.microsoft.com/fwlink/?LinkId=392237");
+            }
+
+            fixture = new Fixture().Customize(new AutoMoqCustomization());
+            fixture.Inject(s_eventTable);
+            serializer = new JsonMessageSerializer();
+            messageBus = Mock.Of<IMessageBus>();
+            sut = new AzureEventCorrector(s_eventTable, serializer, messageBus);
+        }
+
+        [TestMethod]
+        public void sut_implements_IAzureEventCorrector()
+        {
+            sut.Should().BeAssignableTo<IAzureEventCorrector>();
+        }
+
+        [TestMethod]
+        public void class_has_guard_clauses()
+        {
+            var assertion = new GuardClauseAssertion(fixture);
+            assertion.Verify(typeof(AzureEventCorrector));
+        }
+
+        [TestMethod]
+        public async Task CorrectEvents_persists_unpersisted_pending_events()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            var userCreated = fixture.Create<FakeUserCreated>();
+            var usernameChanged = fixture.Create<FakeUsernameChanged>();
+            var domainEvents = new DomainEvent[] { userCreated, usernameChanged };
+            RaiseEvents(userId, domainEvents);
+
+            var serializer = new JsonMessageSerializer();
+
+            var batchOperation = new TableBatchOperation();
+            domainEvents
+                .Select(e => PendingEventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            batchOperation.Clear();
+            domainEvents
+                .Take(1)
+                .Select(e => EventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            // Act
+            await sut.CorrectEvents<FakeUser>(userId);
+
+            // Assert
+            string partitionKey = EventTableEntity.GetPartitionKey(typeof(FakeUser), userId);
+            var query = new TableQuery<EventTableEntity>().Where($"PartitionKey eq '{partitionKey}'");
+            IEnumerable<object> actual = s_eventTable
+                .ExecuteQuery(query)
+                .Select(e => new
+                {
+                    e.RowKey,
+                    e.SourceType,
+                    e.SourceId,
+                    e.EventType,
+                    e.RaisedAt,
+                    Payload = serializer.Deserialize(e.PayloadJson)
+                });
+
+            IEnumerable<object> expected = domainEvents
+                .Select(e => new
+                {
+                    RowKey = EventTableEntity.GetRowKey(e.Version),
+                    SourceType = typeof(FakeUser).FullName,
+                    SourceId = userId,
+                    EventType = e.GetType().FullName,
+                    e.RaisedAt,
+                    Payload = e
+                });
+
+            actual.ShouldAllBeEquivalentTo(expected);
+        }
+
+        [TestMethod]
+        public async Task CorrectEvents_sends_all_pending_events()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            var userCreated = fixture.Create<FakeUserCreated>();
+            var usernameChanged = fixture.Create<FakeUsernameChanged>();
+            var domainEvents = new DomainEvent[] { userCreated, usernameChanged };
+            RaiseEvents(userId, domainEvents);
+
+            var serializer = new JsonMessageSerializer();
+
+            var batchOperation = new TableBatchOperation();
+            domainEvents
+                .Select(e => PendingEventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            batchOperation.Clear();
+            domainEvents
+                .Take(1)
+                .Select(e => EventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            List<object> batch = null;
+
+            Mock.Get(messageBus)
+                .Setup(x => x.SendBatch(It.IsAny<IEnumerable<object>>()))
+                .Callback<IEnumerable<object>>(b => batch = b.ToList())
+                .Returns(Task.FromResult(true));
+
+            // Act
+            await sut.CorrectEvents<FakeUser>(userId);
+
+            // Assert
+            Mock.Get(messageBus).Verify(
+                x =>
+                x.SendBatch(It.IsAny<IEnumerable<object>>()),
+                Times.Once());
+            batch.Should().OnlyContain(e => e is IDomainEvent);
+            batch.Cast<IDomainEvent>().Should().BeInAscendingOrder(e => e.Version);
+            batch.ShouldAllBeEquivalentTo(domainEvents);
+        }
+
+        [TestMethod]
+        public async Task CorrectEvents_does_not_send_pending_events_if_fails_to_persist()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            var userCreated = fixture.Create<FakeUserCreated>();
+            var usernameChanged = fixture.Create<FakeUsernameChanged>();
+            var domainEvents = new DomainEvent[] { userCreated, usernameChanged };
+            RaiseEvents(userId, domainEvents);
+
+            var serializer = new JsonMessageSerializer();
+
+            var batchOperation = new TableBatchOperation();
+            domainEvents
+                .Select(e => PendingEventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            var eventTableMock = new Mock<CloudTable>(
+                s_eventTable.Uri,
+                s_storageAccount.Credentials)
+            {
+                CallBase = true
+            };
+            var sut = new AzureEventCorrector(
+                eventTableMock.Object, serializer, messageBus);
+
+            eventTableMock
+                .Setup(x => x.ExecuteBatchAsync(It.IsAny<TableBatchOperation>()))
+                .ThrowsAsync(new StorageException());
+
+            // Act
+            try
+            {
+                await sut.CorrectEvents<FakeUser>(userId);
+            }
+            catch (StorageException)
+            {
+            }
+
+            // Assert
+            Mock.Get(messageBus).Verify(
+                x => x.SendBatch(It.IsAny<IEnumerable<object>>()),
+                Times.Never());
+        }
+
+        [TestMethod]
+        public async Task CorrectEvents_deletes_all_pending_events()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            var userCreated = fixture.Create<FakeUserCreated>();
+            var usernameChanged = fixture.Create<FakeUsernameChanged>();
+            var domainEvents = new DomainEvent[] { userCreated, usernameChanged };
+            RaiseEvents(userId, domainEvents);
+
+            var serializer = new JsonMessageSerializer();
+
+            var batchOperation = new TableBatchOperation();
+            domainEvents
+                .Select(e => PendingEventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            batchOperation.Clear();
+            domainEvents
+                .Take(1)
+                .Select(e => EventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            // Act
+            await sut.CorrectEvents<FakeUser>(userId);
+
+            // Assert
+            string partitionKey = PendingEventTableEntity.GetPartitionKey(typeof(FakeUser), userId);
+            var query = new TableQuery<PendingEventTableEntity>().Where($"PartitionKey eq '{partitionKey}'");
+            List<PendingEventTableEntity> actual = s_eventTable.ExecuteQuery(query).ToList();
+            actual.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public async Task CorrectEvents_does_not_delete_pending_events_if_fails_to_send()
+        {
+            // Arrange
+            var userId = Guid.NewGuid();
+
+            var userCreated = fixture.Create<FakeUserCreated>();
+            var usernameChanged = fixture.Create<FakeUsernameChanged>();
+            var domainEvents = new DomainEvent[] { userCreated, usernameChanged };
+            RaiseEvents(userId, domainEvents);
+
+            var serializer = new JsonMessageSerializer();
+
+            var batchOperation = new TableBatchOperation();
+            domainEvents
+                .Select(e => PendingEventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            batchOperation.Clear();
+            domainEvents
+                .Take(1)
+                .Select(e => EventTableEntity.FromDomainEvent<FakeUser>(e, serializer))
+                .ForEach(batchOperation.Insert);
+            await s_eventTable.ExecuteBatchAsync(batchOperation);
+
+            Mock.Get(messageBus)
+                .Setup(x => x.SendBatch(It.IsAny<IEnumerable<object>>()))
+                .Throws(new InvalidOperationException());
+
+            // Act
+            try
+            {
+                await sut.CorrectEvents<FakeUser>(userId);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+
+            // Assert
+            string partitionKey = PendingEventTableEntity.GetPartitionKey(typeof(FakeUser), userId);
+            var query = new TableQuery<PendingEventTableEntity>().Where($"PartitionKey eq '{partitionKey}'");
+            IEnumerable<object> actual = s_eventTable
+                .ExecuteQuery(query)
+                .Select(e => new
+                {
+                    e.RowKey,
+                    e.SourceType,
+                    e.SourceId,
+                    e.EventType,
+                    e.RaisedAt,
+                    Payload = serializer.Deserialize(e.PayloadJson)
+                });
+
+            IEnumerable<object> expected = domainEvents
+                .Select(e => new
+                {
+                    RowKey = EventTableEntity.GetRowKey(e.Version),
+                    SourceType = typeof(FakeUser).FullName,
+                    SourceId = userId,
+                    EventType = e.GetType().FullName,
+                    e.RaisedAt,
+                    Payload = e
+                });
+
+            actual.ShouldAllBeEquivalentTo(expected);
+        }
+
+        private void RaiseEvents(Guid sourceId, params DomainEvent[] events)
+        {
+            RaiseEvents(sourceId, 0, events);
+        }
+
+        private void RaiseEvents(
+            Guid sourceId, int versionOffset, params DomainEvent[] events)
+        {
+            for (int i = 0; i < events.Length; i++)
+            {
+                events[i].SourceId = sourceId;
+                events[i].Version = versionOffset + i + 1;
+                events[i].RaisedAt = DateTimeOffset.Now;
+            }
+        }
+    }
+}
