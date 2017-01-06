8c487cafb50a3ea4b7eb552a0e4c105f9da3ddea
Gyuwon Yi <gyuwon@live.com>
2017-01-06 22:46:50 +0900

    IAzureEventCorrector 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing.Azure/EventSourcing/Azure/IAzureEventCorrector.cs
@@ -0,0 +1,11 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Azure
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IAzureEventCorrector
+    {
+        Task CorrectEvents<T>(Guid sourceId)
+            where T : class, IEventSourced;
+    }
+}
