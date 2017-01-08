458e3af807bddcc1032ca297234877443e3543fe
Gyuwon Yi <gyuwon@live.com>
2017-01-08 09:38:05 +0900

    IAzureEventStore 인터페이스를 추가합니다.

---
+++ source/RA.EventSourcing.Azure/EventSourcing/Azure/IAzureEventStore.cs
@@ -0,0 +1,16 @@
+﻿namespace ReactiveArchitecture.EventSourcing.Azure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IAzureEventStore
+    {
+        Task SeveEvents<T>(IEnumerable<IDomainEvent> events)
+            where T : class, IEventSourced;
+
+        Task<IEnumerable<IDomainEvent>> LoadEvents<T>(
+            Guid sourceId, int afterVersion = default(int))
+            where T : class, IEventSourced;
+    }
+}
