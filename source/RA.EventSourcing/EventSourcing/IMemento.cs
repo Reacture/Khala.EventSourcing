83d65d007b668cd7f7c80dcbfd752e7b61223bc2
Gyuwon Yi <gyuwon@live.com>
2017-01-13 02:11:32 +0900

    IMementoStore 인터페이스를 추가합니다.

--- source/RA.EventSourcing/EventSourcing/IMemento.cs
+++ source/RA.EventSourcing/EventSourcing/IMemento.cs
@@ -1,7 +1,11 @@
 ﻿namespace ReactiveArchitecture.EventSourcing
 {
+    using System;
+
     public interface IMemento
     {
+        Guid SourceId { get; }
+
         int Version { get; }
     }
 }
---
+++ source/RA.EventSourcing/EventSourcing/IMementoStore.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactiveArchitecture.EventSourcing
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IMementoStore
+    {
+        Task Save<T>(IMemento memento);
+
+        IMemento Find<T>(Guid sourceId);
+    }
+}
