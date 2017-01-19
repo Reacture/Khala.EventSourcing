cf63558ca164effb8863f002b190f51c41e83c7a
Gyuwon Yi <gyuwon@live.com>
2017-01-20 02:34:28 +0900

    nuget 설정을 추가합니다.

--- source/RA.EventSourcing.Sql/Properties/AssemblyInfo.cs
+++ source/RA.EventSourcing.Sql/Properties/AssemblyInfo.cs
@@ -1,35 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("RA.EventSourcing.Sql")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RA.EventSourcing.Sql")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Reactive Essentials")]
+[assembly: AssemblyTitle("Event Sourcing SQL")]
+[assembly: AssemblyProduct("Event Sourcing")]
+[assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d0d8c3ca-5084-4b26-a256-fb73a1ee0feb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
