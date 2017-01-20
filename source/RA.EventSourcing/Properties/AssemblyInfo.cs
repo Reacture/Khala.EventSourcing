f8b201dfe254a36dbf25438a0a51682e2538c65c
Gyuwon Yi <gyuwon@live.com>
2017-01-20 12:08:07 +0900

    v0.1.1

--- source/RA.EventSourcing/Properties/AssemblyInfo.cs
+++ source/RA.EventSourcing/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 [assembly: AssemblyProduct("Event Sourcing")]
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
