40a36d012a39022b5e691402450e750d3ed30420
Gyuwon Yi <gyuwon@live.com>
2017-01-20 12:27:53 +0900

    v0.1.2

--- source/RA.EventSourcing.Azure/Properties/AssemblyInfo.cs
+++ source/RA.EventSourcing.Azure/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: Guid("047d8ef5-0e46-4c2c-8d6f-bd970b94fa43")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
