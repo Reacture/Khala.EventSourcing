8f0d62e9a98c7c80aba4dc68c4d98a665f0c93ec
Gyuwon Yi <gyuwon@live.com>
2017-01-25 16:20:13 +0900

    EventSourcingExtensions 클래스를 추가합니다.

---
+++ source/RA.EventSourcing/EventSourcing/EventSourcingExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace ReactiveArchitecture.EventSourcing
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public static class EventSourcingExtensions
+    {
+        public static Task Save<T>(
+            this IEventSourcedRepository<T> repository,
+            T source,
+            CancellationToken cancellationToken)
+            where T : class, IEventSourced
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return repository.Save(source, null, cancellationToken);
+        }
+    }
+}
