188b907ab1b286d2a469c89876bee1b127a7528c
Gyuwon Yi <gyuwon@live.com>
2017-01-30 12:47:41 +0900

    EventSourcingExtensions 확장 메서드 집합 클래스를 보강합니다.

--- source/Arcane.EventSourcing.Tests/EventSourcing/EventSourcingExtensions_features.cs
+++ source/Arcane.EventSourcing.Tests/EventSourcing/EventSourcingExtensions_features.cs
@@ -1,9 +1,13 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Arcane.EventSourcing.Sql;
+using Arcane.FakeDomain;
+using FluentAssertions;
 using Moq;
 using Ploeh.AutoFixture;
 using Ploeh.AutoFixture.AutoMoq;
 using Ploeh.AutoFixture.Idioms;
-using Arcane.FakeDomain;
 using Xunit;
 
 namespace Arcane.EventSourcing
@@ -19,7 +23,7 @@ namespace Arcane.EventSourcing
         }
 
         [Fact]
-        public void Save_relays_to_repository_with_null_correlation()
+        public void Save_relays_with_null_correlation()
         {
             var fixture = new Fixture();
             var repository = Mock.Of<IEventSourcedRepository<FakeUser>>();
@@ -31,6 +35,76 @@ namespace Arcane.EventSourcing
                 x =>
                 x.Save(source, null, CancellationToken.None),
                 Times.Once());
+        }
+
+        [Fact]
+        public void Save_relays_with_none_cancellation_token()
+        {
+            var task = Task.FromResult(true);
+            var source = new FakeUser(Guid.NewGuid(), "foo");
+            var correlationId = Guid.NewGuid();
+            var repository = Mock.Of<IEventSourcedRepository<FakeUser>>(
+                x => x.Save(source, correlationId, CancellationToken.None) == task);
+
+            Task result = repository.Save(source, correlationId);
+
+            Mock.Get(repository).Verify(
+                x =>
+                x.Save(source, correlationId, CancellationToken.None),
+                Times.Once());
+            result.Should().BeSameAs(task);
+        }
+
+        [Fact]
+        public void Save_relays_with_null_correlation_and_none_cancellation_token()
+        {
+            var task = Task.FromResult(true);
+            var source = new FakeUser(Guid.NewGuid(), "foo");
+            var repository = Mock.Of<IEventSourcedRepository<FakeUser>>(
+                x => x.Save(source, null, CancellationToken.None) == task);
+
+            Task result = repository.Save(source);
+
+            Mock.Get(repository).Verify(
+                x =>
+                x.Save(source, null, CancellationToken.None),
+                Times.Once());
+            result.Should().BeSameAs(task);
+        }
+
+        [Fact]
+        public void Find_relays_with_none_cancellation_token()
+        {
+            var source = new FakeUser(Guid.NewGuid(), "foo");
+            var task = Task.FromResult(source);
+            var repository = Mock.Of<IEventSourcedRepository<FakeUser>>(
+                x => x.Find(source.Id, CancellationToken.None) == task);
+
+            Task<FakeUser> result = repository.Find(source.Id);
+
+            Mock.Get(repository).Verify(
+                x => x.Find(source.Id, CancellationToken.None), Times.Once());
+            result.Should().BeSameAs(task);
+        }
+
+        [Fact]
+        public void FindIdByUniqueIndexedProperty_relays_with_none_cancellation_token()
+        {
+            var fixture = new Fixture();
+            var task = fixture.Create<Task<Guid?>>();
+            string name = fixture.Create(nameof(name));
+            string value = fixture.Create(nameof(value));
+            var repository = Mock.Of<ISqlEventSourcedRepository<FakeUser>>(
+                x =>
+                x.FindIdByUniqueIndexedProperty(name, value, CancellationToken.None) == task);
+
+            Task<Guid?> result = repository.FindIdByUniqueIndexedProperty(name, value);
+
+            Mock.Get(repository).Verify(
+                x =>
+                x.FindIdByUniqueIndexedProperty(name, value, CancellationToken.None),
+                Times.Once());
+            result.Should().BeSameAs(task);
         }
     }
 }
--- source/Arcane.EventSourcing/EventSourcing/EventSourcingExtensions.cs
+++ source/Arcane.EventSourcing/EventSourcing/EventSourcingExtensions.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Threading;
     using System.Threading.Tasks;
+    using Sql;
 
     public static class EventSourcingExtensions
     {
@@ -23,6 +24,86 @@
             }
 
             return repository.Save(source, null, cancellationToken);
+        }
+
+        public static Task Save<T>(
+            this IEventSourcedRepository<T> repository,
+            T source,
+            Guid? correlationId)
+            where T : class, IEventSourced
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return repository.Save(source, correlationId, CancellationToken.None);
+        }
+
+        public static Task Save<T>(
+            this IEventSourcedRepository<T> repository,
+            T source)
+            where T : class, IEventSourced
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return repository.Save(source, null, CancellationToken.None);
+        }
+
+        public static Task<T> Find<T>(
+            this IEventSourcedRepository<T> repository,
+            Guid sourceId)
+            where T : class, IEventSourced
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (sourceId == Guid.Empty)
+            {
+                throw new ArgumentException(
+                    $"{nameof(sourceId)} cannot be empty.", nameof(sourceId));
+            }
+
+            return repository.Find(sourceId, CancellationToken.None);
+        }
+
+        public static Task<Guid?> FindIdByUniqueIndexedProperty<T>(
+            this ISqlEventSourcedRepository<T> repository,
+            string name,
+            string value)
+            where T : class, IEventSourced
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return repository.FindIdByUniqueIndexedProperty(name, value, CancellationToken.None);
         }
     }
 }
