ffa525a29067e3a0117f5409cec7305798b011e6
Gyuwon <gyuwon@live.com>
2017-07-21 00:01:42 +0900

    AppBuilderExtensions 클래스를 추가합니다.\
    This resolves #11

---
+++ source/Khala.EventSourcing.Owin/EventSourcing/AppBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Owin.BuilderProperties;
+using Owin;
+
+namespace Khala.EventSourcing
+{
+    public static class AppBuilderExtensions
+    {
+        public static void EnqueuePendingEvents<T>(
+            this IAppBuilder app,
+            IEventSourcedRepository<T> repository)
+            where T : class, IEventSourced
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+
+            if (repository == null)
+                throw new ArgumentNullException(nameof(repository));
+
+            var appProperties = new AppProperties(app.Properties);
+            CancellationToken cancellationToken = appProperties.OnAppDisposing;
+            repository.EventPublisher.EnqueueAll(cancellationToken);
+        }
+    }
+}
---
+++ source/Khala.EventSourcing.Tests/EventSourcing/AppBuilderExtensions_features.cs
@@ -0,0 +1,125 @@
+﻿namespace Khala.EventSourcing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Microsoft.Owin.BuilderProperties;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Owin;
+
+    [TestClass]
+    public class AppBuilderExtensions_features
+    {
+        public class FooAppBuilder : IAppBuilder
+        {
+            public IDictionary<string, object> Properties => throw new NotImplementedException();
+
+            public object Build(Type returnType)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IAppBuilder New()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IAppBuilder Use(object middleware, params object[] args)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class FooRepository : IEventSourcedRepository<IEventSourced>
+        {
+            public IEventPublisher EventPublisher => throw new NotImplementedException();
+
+            public Task<IEventSourced> Find(Guid sourceId, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task Save(IEventSourced source, Guid? correlationId, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class FooEventSourced : IEventSourced
+        {
+            public IEnumerable<IDomainEvent> PendingEvents => throw new NotImplementedException();
+
+            public Guid Id => throw new NotImplementedException();
+
+            public int Version => throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void EnqueuePendingEvents_has_guard_clause_against_null_app_argument()
+        {
+            // Arrange
+            IAppBuilder app = null;
+            IEventSourcedRepository<IEventSourced> respository = new FooRepository();
+
+            // Act
+            object exception = null;
+            try
+            {
+                AppBuilderExtensions.EnqueuePendingEvents(app, respository);
+            }
+            catch (Exception thrown)
+            {
+                exception = thrown;
+            }
+
+            // Assert
+            exception.Should().NotBeNull()
+                .And.BeOfType<ArgumentNullException>()
+                .Which.ParamName.Should().Be("app");
+        }
+
+        [TestMethod]
+        public void EnqueuePendingEvents_has_guard_clause_against_null_repository_argument()
+        {
+            // Arrange
+            IAppBuilder app = new FooAppBuilder();
+            IEventSourcedRepository<IEventSourced> respository = null;
+
+            // Act
+            Action action = () =>
+            AppBuilderExtensions.EnqueuePendingEvents(app, respository);
+
+            // Assert
+            action.ShouldThrow<ArgumentNullException>().Where(x => x.ParamName == "repository");
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public void EnqueuePendingEvents_invokes_EnqueueAll_with_OnAppDisposing_once(bool canceled)
+        {
+            // Arrange
+            IAppBuilder app = Mock.Of<IAppBuilder>(
+                x => x.Properties == new Dictionary<string, object>());
+
+            IEventPublisher eventPublisher = Mock.Of<IEventPublisher>();
+            IEventSourcedRepository<FooEventSourced> repository =
+                Mock.Of<IEventSourcedRepository<FooEventSourced>>(
+                    x => x.EventPublisher == eventPublisher);
+
+            var cancellationToken = new CancellationToken(canceled);
+            var appProperties = new AppProperties(app.Properties);
+            appProperties.OnAppDisposing = cancellationToken;
+
+            // Act
+            app.EnqueuePendingEvents(repository);
+
+            // Assert
+            Mock.Get(eventPublisher)
+                .Verify(x => x.EnqueueAll(cancellationToken), Times.Once());
+        }
+    }
+}
