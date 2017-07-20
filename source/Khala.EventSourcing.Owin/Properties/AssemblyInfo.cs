8ff95e3e5a9ad6bc7ff56504f39261d4f83dc895
Gyuwon <gyuwon@live.com>
2017-07-20 13:01:51 +0900

    Khala.EventSourcing.Owin 프로젝트를 추가합니다.

---
+++ source/Khala.EventSourcing.Owin/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Khala.EventSourcing")]
+[assembly: AssemblyTitle("Khala.EventSourcing.Owin")]
+[assembly: AssemblyCompany("Reacture")]
+[assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
+[assembly: Guid("25f03890-3ce8-4363-bd27-88155d84cba1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
