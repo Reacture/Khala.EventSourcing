210e5c5e0e7e1d9ebf44271db4796a29f09220f6
Gyuwon <gyuwon@live.com>
2017-09-07 14:18:42 +0900

    versioning 0.10.1

--- source/Khala.EventSourcing/Properties/AssemblyInfo.cs
+++ source/Khala.EventSourcing/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyTitle("Khala.EventSourcing")]
 [assembly: AssemblyCompany("Reacture")]
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("0.10.1.0")]
+[assembly: AssemblyFileVersion("0.10.1.0")]
