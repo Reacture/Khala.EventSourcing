47aee97e2ed3cf87b11fb856c7c237792c1a0bce
Gyuwon <gyuwon@live.com>
2017-09-10 10:57:43 +0900

    Khala.EventSourcing.Contracts 프로젝트를 추가합니다.

---
+++ source/Khala.EventSourcing.Contracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Khala.EventSourcing")]
+[assembly: AssemblyTitle("Khala.EventSourcing.Contracts")]
+[assembly: AssemblyCompany("Reacture")]
+[assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
+[assembly: AssemblyVersion("0.11.0.0")]
+[assembly: AssemblyFileVersion("0.11.0.0")]
