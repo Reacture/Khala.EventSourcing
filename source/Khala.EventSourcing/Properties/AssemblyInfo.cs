5057466b03025eddb9100da7afc2c4b43b98f906
Gyuwon Yi <gyuwon@live.com>
2017-10-02 21:08:01 +0900

    Portable 프로젝트를 .NET Standard 프로젝트로 변환합니다.

--- source/Khala.EventSourcing/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyProduct("Khala.EventSourcing")]
-[assembly: AssemblyTitle("Khala.EventSourcing.Core")]
-[assembly: AssemblyCompany("Reacture")]
-[assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
-[assembly: AssemblyVersion("0.13.0.0")]
-[assembly: AssemblyFileVersion("0.13.0.0")]
