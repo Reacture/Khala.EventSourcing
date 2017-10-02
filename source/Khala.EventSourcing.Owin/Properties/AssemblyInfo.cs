388d9c0ae659e0dcab8592c32ce33cc396d1d5ed
Gyuwon Yi <gyuwon@live.com>
2017-10-02 21:20:30 +0900

    versioning 0.14.0

--- source/Khala.EventSourcing.Owin/Properties/AssemblyInfo.cs
+++ source/Khala.EventSourcing.Owin/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Reacture")]
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: Guid("25f03890-3ce8-4363-bd27-88155d84cba1")]
-[assembly: AssemblyVersion("0.13.0.0")]
-[assembly: AssemblyFileVersion("0.13.0.0")]
+[assembly: AssemblyVersion("0.14.0.0")]
+[assembly: AssemblyFileVersion("0.14.0.0")]
 [assembly: ComVisible(false)]
