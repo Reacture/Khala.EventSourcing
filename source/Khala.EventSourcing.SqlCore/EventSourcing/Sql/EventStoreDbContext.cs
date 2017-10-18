a5a6b3eb95bd9b00435ca1c53ce5518ed287ca0d
Gyuwon <gyuwon@live.com>
2017-10-18 20:00:29 +0900

    Aggregate 클래스 코드를 .NET Standard 프로젝트와 공유합니다.

---
+++ source/Khala.EventSourcing.SqlCore/EventSourcing/Sql/EventStoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace Khala.EventSourcing.Sql
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class EventStoreDbContext : DbContext
+    {
+        public EventStoreDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aggregate> Aggregates { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Aggregate>().HasIndex(nameof(Aggregate.AggregateId)).IsUnique();
+        }
+    }
+}
---
+++ source/Khala.EventSourcing.Tests.Core/EventSourcing/Sql/EventStoreDbContext_specs.cs
@@ -0,0 +1,43 @@
+﻿namespace Khala.EventSourcing.Sql
+{
+    using FluentAssertions;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using Xunit;
+
+    public class EventStoreDbContext_specs
+    {
+        private readonly DbContextOptions _dbContextOptions;
+
+        public EventStoreDbContext_specs()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder()
+                .UseInMemoryDatabase(nameof(EventStoreDbContext_specs))
+                .Options;
+        }
+
+        [Fact]
+        public void sut_inherits_DbContext()
+        {
+            typeof(EventStoreDbContext).BaseType.Should().Be(typeof(DbContext));
+        }
+
+        [Fact]
+        public void model_has_Aggregate_entity()
+        {
+            var sut = new EventStoreDbContext(_dbContextOptions);
+            IEntityType actual = sut.Model.FindEntityType(typeof(Aggregate));
+            actual.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Aggregate_entity_has_index_for_AggregateId()
+        {
+            var sut = new EventStoreDbContext(_dbContextOptions);
+            IEntityType entity = sut.Model.FindEntityType(typeof(Aggregate));
+            IIndex actual = entity.FindIndex(entity.FindProperty("AggregateId"));
+            actual.Should().NotBeNull();
+            actual.IsUnique.Should().BeTrue();
+        }
+    }
+}
