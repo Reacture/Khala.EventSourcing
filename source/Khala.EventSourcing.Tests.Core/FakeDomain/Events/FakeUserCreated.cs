ff73ff1e69a41ef011873bacd0917c206fa75606
Gyuwon <gyuwon@live.com>
2017-10-19 17:49:40 +0900

    Khala.EventSourcing.Tests.Core 프로젝트에 FakeDomain 네임스페이스를 추가합니다.

---
+++ source/Khala.EventSourcing.Tests.Core/FakeDomain/Events/FakeUserCreated.cs
@@ -0,0 +1,24 @@
+﻿namespace Khala.FakeDomain.Events
+{
+    using System;
+    using System.Collections.Generic;
+    using Khala.EventSourcing.Sql;
+    using Newtonsoft.Json;
+
+    public class FakeUserCreated : FakeDomainEvent, IUniqueIndexedDomainEvent
+    {
+        public string Username { get; set; } = Guid.NewGuid().ToString();
+
+        [JsonIgnore]
+        public IReadOnlyDictionary<string, string> UniqueIndexedProperties
+        {
+            get
+            {
+                return new Dictionary<string, string>
+                {
+                    ["Username"] = Username
+                };
+            }
+        }
+    }
+}
---
+++ source/Khala.EventSourcing.Tests.Core/FakeDomain/FakeUserMemento.cs
@@ -0,0 +1,11 @@
+﻿namespace Khala.FakeDomain
+{
+    using Khala.EventSourcing;
+
+    public class FakeUserMemento : IMemento
+    {
+        public int Version { get; set; }
+
+        public string Username { get; set; }
+    }
+}
