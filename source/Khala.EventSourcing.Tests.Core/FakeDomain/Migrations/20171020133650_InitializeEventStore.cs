374c84418256ac0442403a5e7670603df92fbbcb
Gyuwon Yi <gyuwon@live.com>
2017-10-20 22:54:24 +0900

    테스트 데이터베이스 마이그레이션을 추가합니다.

---
+++ source/Khala.EventSourcing.Tests.Core/FakeDomain/Migrations/20171020133650_InitializeEventStore.cs
@@ -0,0 +1,125 @@
+﻿namespace Khala.FakeDomain.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class InitializeEventStore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aggregates",
+                columns: table => new
+                {
+                    SequenceId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AggregateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AggregateType = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aggregates", x => x.SequenceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Correlations",
+                columns: table => new
+                {
+                    AggregateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    HandledAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Correlations", x => new { x.AggregateId, x.CorrelationId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PendingEvents",
+                columns: table => new
+                {
+                    AggregateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false),
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    EventJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PendingEvents", x => new { x.AggregateId, x.Version });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersistentEvents",
+                columns: table => new
+                {
+                    SequenceId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AggregateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    EventJson = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RaisedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersistentEvents", x => x.SequenceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UniqueIndexedProperties",
+                columns: table => new
+                {
+                    AggregateType = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    PropertyName = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    PropertyValue = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    AggregateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniqueIndexedProperties", x => new { x.AggregateType, x.PropertyName, x.PropertyValue });
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aggregates_AggregateId",
+                table: "Aggregates",
+                column: "AggregateId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersistentEvents_AggregateId_Version",
+                table: "PersistentEvents",
+                columns: new[] { "AggregateId", "Version" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UniqueIndexedProperties_AggregateId_PropertyName",
+                table: "UniqueIndexedProperties",
+                columns: new[] { "AggregateId", "PropertyName" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aggregates");
+
+            migrationBuilder.DropTable(
+                name: "Correlations");
+
+            migrationBuilder.DropTable(
+                name: "PendingEvents");
+
+            migrationBuilder.DropTable(
+                name: "PersistentEvents");
+
+            migrationBuilder.DropTable(
+                name: "UniqueIndexedProperties");
+        }
+    }
+}
