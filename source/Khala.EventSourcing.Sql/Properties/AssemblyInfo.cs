d7284c36fe986beb7063815bb14cc148c9e83e6a
Gyuwon Yi <gyuwon@live.com>
2018-01-08 00:13:12 +0900

    versioning 0.17.0

--- source/Khala.EventSourcing.Sql/Properties/AssemblyInfo.cs
+++ source/Khala.EventSourcing.Sql/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Reacture")]
 [assembly: AssemblyCopyright("Copyright Gyuwon 2017")]
 [assembly: Guid("dacbbd4b-4064-4f04-a056-2479ed98114d")]
-[assembly: AssemblyVersion("0.16.2.0")]
-[assembly: AssemblyFileVersion("0.16.2.0")]
+[assembly: AssemblyVersion("0.17.0.0")]
+[assembly: AssemblyFileVersion("0.17.0.0")]
 [assembly: ComVisible(false)]
