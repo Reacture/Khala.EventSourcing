be7a9122162dd7a199587adafb1e966c7df21b6a
Gyuwon Yi <gyuwon@live.com>
2018-01-07 12:50:34 +0900

    PersistentEvent 클래스를 추가한다

---
+++ source/Khala.EventSourcing.Azure/EventSourcing/Azure/PersistentEvent.cs
@@ -0,0 +1,77 @@
+﻿namespace Khala.EventSourcing.Azure
+{
+    using System;
+    using Khala.Messaging;
+    using Microsoft.WindowsAzure.Storage.Table;
+
+    public class PersistentEvent : TableEntity
+    {
+        public int Version { get; set; }
+
+        public string EventType { get; set; }
+
+        public DateTimeOffset RaisedAt { get; set; }
+
+        public Guid MessageId { get; set; }
+
+        public string EventJson { get; set; }
+
+        public Guid? OperationId { get; set; }
+
+        public Guid? CorrelationId { get; set; }
+
+        public string Contributor { get; set; }
+
+        public static string GetPartitionKey(Type sourceType, Guid sourceId)
+        {
+            if (sourceType == null)
+            {
+                throw new ArgumentNullException(nameof(sourceType));
+            }
+
+            if (sourceId == Guid.Empty)
+            {
+                throw new ArgumentException("Value cannot be empty.", nameof(sourceId));
+            }
+
+            return $"{sourceType.Name}-{sourceId:n}";
+        }
+
+        public static string GetRowKey(int version) => $"{version:D10}";
+
+        public static PersistentEvent Create(
+            Type sourceType,
+            Envelope<IDomainEvent> envelope,
+            IMessageSerializer serializer)
+        {
+            if (sourceType == null)
+            {
+                throw new ArgumentNullException(nameof(sourceType));
+            }
+
+            if (envelope == null)
+            {
+                throw new ArgumentNullException(nameof(envelope));
+            }
+
+            if (serializer == null)
+            {
+                throw new ArgumentNullException(nameof(serializer));
+            }
+
+            return new PersistentEvent
+            {
+                PartitionKey = GetPartitionKey(sourceType, envelope.Message.SourceId),
+                RowKey = GetRowKey(envelope.Message.Version),
+                Version = envelope.Message.Version,
+                EventType = envelope.Message.GetType().FullName,
+                RaisedAt = envelope.Message.RaisedAt,
+                MessageId = envelope.MessageId,
+                EventJson = serializer.Serialize(envelope.Message),
+                OperationId = envelope.OperationId,
+                CorrelationId = envelope.CorrelationId,
+                Contributor = envelope.Contributor,
+            };
+        }
+    }
+}
---
+++ source/Khala.EventSourcing.Tests.Core/EventSourcing/Azure/PersistentEvent_specs.cs
@@ -0,0 +1,251 @@
+﻿namespace Khala.EventSourcing.Azure
+{
+    using System;
+    using System.Reflection;
+    using AutoFixture;
+    using AutoFixture.AutoMoq;
+    using AutoFixture.Idioms;
+    using FluentAssertions;
+    using Khala.Messaging;
+    using Microsoft.WindowsAzure.Storage.Table;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class PersistentEvent_specs
+    {
+        private readonly ITestOutputHelper _output;
+
+        public PersistentEvent_specs(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public class SomeDomainEvent : DomainEvent
+        {
+            public int Foo { get; set; }
+
+            public string Bar { get; set; }
+        }
+
+        [Fact]
+        public void sut_inherits_TableEntity()
+        {
+            typeof(PersistentEvent).BaseType.Should().Be(typeof(TableEntity));
+        }
+
+        [Fact]
+        public void GetPartitionKey_returns_combination_of_source_type_name_and_source_id()
+        {
+            IFixture fixture = new Fixture().Customize(new AutoMoqCustomization());
+            Type sourceType = fixture.Create<IEventSourced>().GetType();
+            Guid sourceId = fixture.Create<Guid>();
+
+            string actual = PersistentEvent.GetPartitionKey(sourceType, sourceId);
+
+            actual.Should().Be($"{sourceType.Name}-{sourceId:n}");
+        }
+
+        [Fact]
+        public void GetPartitionKey_has_guard_clauses()
+        {
+            MethodInfo mut = typeof(PersistentEvent).GetMethod("GetPartitionKey");
+            new GuardClauseAssertion(new Fixture()).Verify(mut);
+        }
+
+        [Fact]
+        public void GetRowKey_returns_formatted_version()
+        {
+            int version = new Fixture().Create<int>();
+            string actual = PersistentEvent.GetRowKey(version);
+            actual.Should().Be($"{version:D10}");
+        }
+
+        [Fact]
+        public void Create_returns_PersistentEvent_instance()
+        {
+            IFixture fixture = new Fixture();
+            fixture.Register<IDomainEvent>(() => fixture.Create<SomeDomainEvent>());
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                fixture.Create<Envelope<IDomainEvent>>(),
+                new JsonMessageSerializer());
+
+            actual.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Create_has_guard_clauses()
+        {
+            MethodInfo mut = typeof(PersistentEvent).GetMethod("Create");
+            IFixture builder = new Fixture().Customize(new AutoMoqCustomization());
+            new GuardClauseAssertion(builder).Verify(mut);
+        }
+
+        [Fact]
+        public void Create_sets_PartitionKey_correctly()
+        {
+            IFixture fixture = new Fixture();
+            Type sourceType = fixture.Create<Type>();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            _output.WriteLine($"SourceId: {domainEvent.SourceId}");
+            fixture.Inject<IDomainEvent>(domainEvent);
+
+            var actual = PersistentEvent.Create(
+                sourceType,
+                fixture.Create<Envelope<IDomainEvent>>(),
+                new JsonMessageSerializer());
+
+            actual.PartitionKey.Should().Be(PersistentEvent.GetPartitionKey(sourceType, domainEvent.SourceId));
+        }
+
+        [Fact]
+        public void Create_sets_RowKey_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            _output.WriteLine($"Version: {domainEvent.Version}");
+            fixture.Inject<IDomainEvent>(domainEvent);
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                fixture.Create<Envelope<IDomainEvent>>(),
+                new JsonMessageSerializer());
+
+            actual.RowKey.Should().Be(PersistentEvent.GetRowKey(domainEvent.Version));
+        }
+
+        [Fact]
+        public void Create_sets_Version_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            _output.WriteLine($"Version: {domainEvent.Version}");
+            fixture.Inject<IDomainEvent>(domainEvent);
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                fixture.Create<Envelope<IDomainEvent>>(),
+                new JsonMessageSerializer());
+
+            actual.Version.Should().Be(domainEvent.Version);
+        }
+
+        [Fact]
+        public void Create_sets_EventType_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                fixture.Create<Envelope<IDomainEvent>>(),
+                new JsonMessageSerializer());
+
+            actual.EventType.Should().Be(typeof(SomeDomainEvent).FullName);
+        }
+
+        [Fact]
+        public void Create_sets_RaisedAt_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+            _output.WriteLine($"RaisedAt: {domainEvent.RaisedAt}");
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                fixture.Create<Envelope<IDomainEvent>>(),
+                new JsonMessageSerializer());
+
+            actual.RaisedAt.Should().Be(domainEvent.RaisedAt);
+        }
+
+        [Fact]
+        public void Create_sets_MessageId_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+            Envelope<IDomainEvent> envelope = fixture.Create<Envelope<IDomainEvent>>();
+            _output.WriteLine($"MessageId: {envelope.MessageId}");
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                envelope,
+                new JsonMessageSerializer());
+
+            actual.MessageId.Should().Be(envelope.MessageId);
+        }
+
+        [Fact]
+        public void Create_sets_EventJson_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+            var serializer = new JsonMessageSerializer();
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                fixture.Create<Envelope<IDomainEvent>>(),
+                serializer);
+
+            object restored = serializer.Deserialize(actual.EventJson);
+            restored.Should().BeOfType<SomeDomainEvent>();
+            restored.ShouldBeEquivalentTo(domainEvent);
+        }
+
+        [Fact]
+        public void Create_sets_OperationId_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+            Envelope<IDomainEvent> envelope = fixture.Create<Envelope<IDomainEvent>>();
+            _output.WriteLine($"OperationId: {envelope.OperationId}");
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                envelope,
+                new JsonMessageSerializer());
+
+            actual.OperationId.Should().Be(envelope.OperationId);
+        }
+
+        [Fact]
+        public void Create_sets_CorrelationId_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+            Envelope<IDomainEvent> envelope = fixture.Create<Envelope<IDomainEvent>>();
+            _output.WriteLine($"CorrelationId: {envelope.CorrelationId}");
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                envelope,
+                new JsonMessageSerializer());
+
+            actual.CorrelationId.Should().Be(envelope.CorrelationId);
+        }
+
+        [Fact]
+        public void Create_sets_Contributor_correctly()
+        {
+            IFixture fixture = new Fixture();
+            SomeDomainEvent domainEvent = fixture.Create<SomeDomainEvent>();
+            fixture.Inject<IDomainEvent>(domainEvent);
+            Envelope<IDomainEvent> envelope = fixture.Create<Envelope<IDomainEvent>>();
+            _output.WriteLine($"Contributor: {envelope.Contributor}");
+
+            var actual = PersistentEvent.Create(
+                fixture.Create<Type>(),
+                envelope,
+                new JsonMessageSerializer());
+
+            actual.Contributor.Should().Be(envelope.Contributor);
+        }
+    }
+}
