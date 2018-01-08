b6ac120c9778f21d5e68f774c4e65a4bc865784b
Gyuwon <gyuwon@live.com>
2018-01-08 18:28:57 +0900

    EventSourcingExtensions 클래스를 추가한다

---
+++ source/Khala.EventSourcing.Abstraction/EventSourcing/EventSourcingExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Khala.EventSourcing
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Khala.Messaging;
+
+    public static class EventSourcingExtensions
+    {
+        public static Task SaveAndPublish<T>(
+            this IEventSourcedRepository<T> repository,
+            T source,
+            IEnvelope correlation,
+            CancellationToken cancellationToken = default)
+            where T : class, IEventSourced
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(nameof(repository));
+            }
+
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (correlation == null)
+            {
+                throw new ArgumentNullException(nameof(correlation));
+            }
+
+            return repository.SaveAndPublish(
+                source,
+                correlation.OperationId,
+                correlation.MessageId,
+                correlation.Contributor,
+                cancellationToken);
+        }
+    }
+}
---
+++ source/Khala.EventSourcing.Tests.Core/EventSourcing/EventSourcingExtensions_specs.cs
@@ -0,0 +1,50 @@
+﻿namespace Khala.EventSourcing
+{
+    using System.Threading;
+    using AutoFixture;
+    using AutoFixture.AutoMoq;
+    using AutoFixture.Idioms;
+    using AutoFixture.Kernel;
+    using Khala.FakeDomain;
+    using Khala.Messaging;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+
+    [TestClass]
+    public class EventSourcingExtensions_specs
+    {
+        [TestMethod]
+        public void sut_has_guard_clauses()
+        {
+            IFixture builder = new Fixture().Customize(new AutoMoqCustomization());
+            new GuardClauseAssertion(builder).Verify(typeof(EventSourcingExtensions));
+        }
+
+        [TestMethod]
+        public void SaveAndPublish_with_Envelope_relays_correctly()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var factory = new MethodInvoker(new GreedyConstructorQuery());
+            fixture.Customize<Envelope>(c => c.FromFactory(factory));
+            IEnvelope correlation = fixture.Create<Envelope>();
+            FakeUser source = fixture.Create<FakeUser>();
+            CancellationToken cancellationToken = new CancellationTokenSource().Token;
+            IEventSourcedRepository<FakeUser> repository = Mock.Of<IEventSourcedRepository<FakeUser>>();
+
+            // Act
+            repository.SaveAndPublish(source, correlation, cancellationToken);
+
+            // Assert
+            Mock.Get(repository).Verify(
+                x =>
+                x.SaveAndPublish(
+                    source,
+                    correlation.OperationId,
+                    correlation.MessageId,
+                    correlation.Contributor,
+                    cancellationToken),
+                Times.Once());
+        }
+    }
+}
